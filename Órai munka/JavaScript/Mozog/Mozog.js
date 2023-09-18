cf59737d9c242d7e39030f9c22e57bc0d4542ff0
SzilagyiZsolt <zsoltika20030@gmail.com>
2023-09-18 12:48:15 +0200

    Szamol

--- Órai munka/JavaScript/Mozog/Mozog.js
+++ Órai munka/JavaScript/Mozog/Mozog.js
@@ -1,11 +1,22 @@
 const g5=document.querySelector('#g5');
-const szel=window.screen.width;
-const mag=window.screen.height;
+//const szel=window.screen.width;
+//const mag=window.screen.height;
 
-g5.addEventListener("mouseenter",mozgat);
+g5.addEventListener("mouseover",change);
 
-function mozgat(){
-    X=Math.random()*szel;
-    Y=Math.random()*mag;
-    console.log(X,Y);
+function change()
+{
+    var i = Math.floor(Math.random()*500)+1;
+    var j = Math.floor(Math.random()*500)+1;
+    g5.style.left = i+"px";
+    g5.style.top = j+"px";
 }
+//function mozgat()
+//{
+//    X=Math.random()*szel;
+//    Y=Math.random()*mag;
+//    console.log(X,Y);
+//    g5.style.position="absolute";
+//    g5.style.clientX= X+'px';
+//    g5.style.clientY= Y+'px';
+//}//

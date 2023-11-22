aacda38d6257d094dce4508b900aa32edeba1fe9
SzilagyiZsolt <zsoltika20030@gmail.com>
2023-11-22 08:04:49 +0100

    20231122

--- Órai munka/AJAX/fetch.js
+++ Órai munka/AJAX/fetch.js
@@ -1,32 +1,124 @@
-const inputid=document.querySelector("#id");
-const inputusername=document.querySelector("#username");
-const inputdarab=document.querySelector("#darab");
-const buttonread=document.querySelector("#read");
-const body=document.getElementsByTagName("body")[0];
-const cards=document.querySelectorAll("#cards");
+const inputid = document.querySelector("#id");
+const inputUsername = document.querySelector("#username");
+const inputDarab = document.querySelector("#darab");
+const buttonRead = document.querySelector("#read");
+const divCards = document.querySelector("#cards");
+const create = document.querySelector("#create");
+const update = document.querySelector("#update");
+const Delete = document.querySelector("#delete");
 
-body.addEventListener("load", getAllUsers); 
-buttonread.addEventListener("click",getAllUsers);
+window.addEventListener("load", getAllUsers); //-- a lap betöltésekor is ...
+buttonRead.addEventListener("click", getAllUsers);
 
-async function getAllUsers(){
-    let endpoint="https://retoolapi.dev/Hfa9uy/data";
-    //let head={method: "GET", mode: "no-cors", cache:"no-cache", headers: {"Content-Type": "application/json"}, body: JSON.stringify()};
-    try{
+async function getAllUsers() {
+    let endpoint = "https://retoolapi.dev/Hfa9uy/data";
+    try {
         const response = await fetch(endpoint);
         const users = await response.json();
-        showAllUsers(users);
+        showAllUsers(users);    
+    } catch (error) {
+        console.log(error);
+    }   
+}
+
+function showAllUsers(users){
+    let html="";
+    users.forEach(
+        user => { html += 
+        `
+        <div class="card" style="width: 18rem;">
+        <img src="noimages.png" class="card-img-top" alt="noimage.jpg">
+            <div class="card-body">
+                <h5 class="card-title">${user.id}. ${user.username}</h5>
+                <p class="card-text">${user.darab}</p>
+                <button href="#" class="btn btn-primary" onclick="betoltInputMezobe(${user.id})">Kiválaszt</button>
+            </div>
+        </div>
+        `;
+    });
+    divCards.innerHTML=html;
+}
+async function betoltInputMezobe(id){
+    let url=`https://retoolapi.dev/Hfa9uy/data/${id}`;
+    try {
+        const response = await fetch(url);
+        const users = await response.json();
+        inputid.value=users.id;
+        inputUsername.value=users.username;
+        inputDarab.value=users.darab;
+        location.href="#formEleje";
+    } catch (error) {
+        console.log(error);
+    }
+    fetch(url);
+}
+
+create.addEventListener("click", async ()=>{
+    let url=`https://retoolapi.dev/Hfa9uy/data`;
+    let data={
+        username: inputUsername.value,
+        darab: inputDarab.value
+    };
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const user = await response.json();
+        console.log(user);
+        beviteliMezokAlap();
+        getAllUsers();
     }
     catch(error){
         console.log(error);
     }
-}
-function showAllUsers(params){
-    params.forEach(user => {
-        appendcard(user);
-    });
-}
+});
 
-function appendcard(user){
-    cards
-    card="<div class=\"card\" style=\"width: 18rem;\"><img src=\"noimages.png\" class=\"card-img-top\" alt=\"...\"><div class=\"card-body\"><h5 class=\"card-title\">Card title</h5><h5>"+user.id+"</h5>";
+Delete.addEventListener("click", async ()=>{
+    let url=`https://retoolapi.dev/Hfa9uy/data/${inputid.value}`;
+    try {
+        const response = await fetch(url, {
+            method: "DELETE"
+        });
+        const user = await response.json();
+        console.log(user);
+        beviteliMezokAlap();
+        getAllUsers();
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+update.addEventListener("click", async ()=>{
+    let url=`https://retoolapi.dev/Hfa9uy/data/${inputid.value}`;
+    let data={
+        username: inputUsername.value,
+        darab: inputDarab.value
+    };
+    try {
+        const response = await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const user = await response.json();
+        console.log(user);
+        beviteliMezokAlap();
+        getAllUsers();
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+function beviteliMezokAlap(){
+    inputid.value="";
+    inputDarab.value="1";
+    inputUsername.value="";
 }
